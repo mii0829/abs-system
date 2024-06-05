a3ae8bd1e599cdbc24239cbd588e846e4b44c0cd
mii0829 <kanokanosakusaku@gmail.com>
2024-06-05 17:56:50 +0900

    データの更新日時を夜中の０時に設定

--- config/clock.rb
+++ config/clock.rb
@@ -9,7 +9,7 @@ module Clockwork
     puts "Running #{job}"
   end
 
-  every(1.day, 'Update isRenting', at: '17:30') do
+  every(1.day, 'Update isRenting', at: '00:00') do #毎日0時にreserveを実行関数を実行（isRentingの値をアップデート）
     Reserve.update_is_renting_for_all
   end
 end
